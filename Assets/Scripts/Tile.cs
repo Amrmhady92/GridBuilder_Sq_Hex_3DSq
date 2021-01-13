001a4d8d56340b3ecbad7e87a4b35acf7ac19037
AmrMHady92 <amrmhady92@gmail.com>
2021-01-14 00:21:38 +0100

    cleaning session

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -24,8 +24,6 @@ public class Tile : MonoBehaviour
     public static int depth = 1;
     private List<Tile> tiles;
 
-
-    private UnityEvent<bool> lightActivatorListener;
     private void Awake()
     {
         MeshRenderer mr = this.GetComponentInChildren<MeshRenderer>();
@@ -79,12 +77,6 @@ public class Tile : MonoBehaviour
             neighbours[(int)NeighborDirection.W]  = tiles.Find(t => t.x == (x - 1) && t.z == (z));
             neighbours[(int)NeighborDirection.NW] = tiles.Find(t => t.x == (x - 1) && t.z == (z - 1));
         }
-        lightActivatorListener = new UnityEvent<bool>();
-        for (int i = 0; i < neighbours.Length; i++)
-        {
-            if (neighbours[i] == null) continue;
-            lightActivatorListener.AddListener(neighbours[i].Light);
-        }
     }
 
 
@@ -110,7 +102,6 @@ public class Tile : MonoBehaviour
     private void OnMouseExit()
     {
         selected = false;
-        //LightNeighbours(false, lightDepth);
         if (depth <= 0) return;
         if(tiles != null)
         {
@@ -124,111 +115,97 @@ public class Tile : MonoBehaviour
         }
     }
 
+
+
+
+    //        ///////////
+    //        //
+    //        //
+    //        //
+    //        //
+    //        //   ECOS
+    //        //
+    //        //
+    //        //
+    //        //
+    //        //
+
+
     public void LightNeighbours(bool onoff, int depth)
     {
-        if (depth <= 0) return;
-        //lightActivatorListener.Invoke(onoff, depth - 1);
-        //for (int i = 0; i < neighbours.Length; i++)
-        //{
-        //    if (neighbours[i] != null)
-        //    {
-        //        neighbours[i].Light(onoff, depth - 1);
-        //if (depth > 0)
-        //{
-        //    neighbours[i].LightNeighbours(onoff, depth);
-        //}
-        //    }
-        //}
-
-
+        if (depth <= 0) return; // may use lightDepth for individual
         NewLight(onoff);
     }
     public void NewLight(bool onoff)
     {
-        /*List<Tile> */
 
-        //if (lightDepth != depth)
-        //{
         if (depth < 0)
         {
             depth = 0;
             return;
         }
-        Collider[] hits = Physics.OverlapSphere(this.transform.position + new Vector3(-0.5f, 0, 0), TileBuilder.tileSizeZ * depth);
-        if(hits.Length > 0)
+
+        if (hex)
         {
-            if (tiles != null) tiles.Clear();
-            else tiles = new List<Tile>();
-
-            Tile hitTile;
-            for (int i = 0; i < hits.Length; i++)
+            Collider[] hits = Physics.OverlapSphere(this.transform.position + new Vector3(-0.5f, 0, 0), TileBuilder.tileSizeZ * depth);
+            if (hits.Length > 0)
             {
-                hitTile = hits[i].gameObject.GetComponent<Tile>();
-                if (hitTile != null && hitTile != this) tiles.Add(hitTile);
-            }
+                if (tiles != null) tiles.Clear();
+                else tiles = new List<Tile>();
 
+                Tile hitTile;
+                for (int i = 0; i < hits.Length; i++)
+                {
+                    hitTile = hits[i].gameObject.GetComponent<Tile>();
+                    if (hitTile != null && hitTile != this) tiles.Add(hitTile);
+                }
+
+                //for (int i = 0; i < tiles.Count; i++)
+                //{
+                //    if (tiles[i] == null) continue;
+                //    tiles[i].Light(onoff);
+                //}
+            }
+        }
+        else // square
+        {
+            int minX = x - Mathf.Max(depth, 0);
+            int maxX = x + Mathf.Max(depth, 0);
+            int minZ = z - Mathf.Abs(depth);
+            int maxZ = z + Mathf.Abs(depth);
+            tiles = TileBuilder.tiles.FindAll(t => t.x >= minX && t.x <= maxX && t.z >= minZ && t.z <= maxZ && t.x != x && t.z != z);
+
+
+        }
+        if (tiles != null)
             for (int i = 0; i < tiles.Count; i++)
             {
                 if (tiles[i] == null) continue;
                 tiles[i].Light(onoff);
             }
-        }
 
-        //int minX = z % 2 == 0 ? x - Mathf.Max(depth - 1, 0) : ;//x - Mathf.Abs(depth) : x - Mathf.Max(Mathf.Abs(depth) - 1, 0);
-        //int maxX = z % 2 == 0 ? x + Mathf.Max(depth - 1, 0) : x + depth;// x + Mathf.Max(Mathf.Abs(depth) - 1, 0) : x + Mathf.Abs(depth);
-        //int minZ = z - Mathf.Abs(depth) + 1;
-        //int maxZ = z + Mathf.Abs(depth) + 1;
-        //tiles = TileBuilder.tiles.FindAll(t => t.x >= minX && t.x <= maxX && t.z >= minZ && t.z <= maxZ && t.x != x && t.z != z);
-        //    tiles = TileBuilder.tiles.FindAll(t =>
-        //(t.x >= (x - Mathf.Abs(depth)) &&
-        //(t.x < x + Mathf.Abs(depth)) &&
-        //(t.z >= z - Mathf.Abs(depth)) &&
-        //(t.z < z + Mathf.Abs(depth))
-        //));
-        //lightDepth = depth;
-        //}
-
-        //if (tiles != null)
-        //for (int i = 0; i < tiles.Count; i++)
-        //{
-        //    if (tiles[i] == null) continue;
-        //        tiles[i].Light(onoff);
-        //}
     }
+
     public void Light(bool onoff)
     {
         if (selected) return;
 
-        //if(/*(lit && onoff == false) || (lit == false && onoff)*/ lit != onoff)
-       // {
-           // lit = onoff;
-            if(onoff) litTiles.Add(this);
+        if (onoff) litTiles.Add(this);
 
-            MeshRenderer mr = this.GetComponentInChildren<MeshRenderer>();
-            Vector3 col;
+        MeshRenderer mr = this.GetComponentInChildren<MeshRenderer>();
+        Vector3 col;
 
-            if (mr)
-            {
+        if (mr)
+        {
+            col.x = onoff ? Mathf.Min(1, mr.material.color.r + 0.2f) : Mathf.Max(0, mr.material.color.r - 0.2f);
+            col.y = onoff ? Mathf.Min(1, mr.material.color.g + 0.2f) : Mathf.Max(0, mr.material.color.g - 0.2f);
+            col.z = onoff ? Mathf.Min(1, mr.material.color.b + 0.2f) : Mathf.Max(0, mr.material.color.b - 0.2f);
+            mr.material.color = new Color(col.x, col.y, col.z);
+        }
 
-                col.x = onoff ? Mathf.Min(1, mr.material.color.r + 0.2f) : Mathf.Max(0, mr.material.color.r - 0.2f);
-                col.y = onoff ? Mathf.Min(1, mr.material.color.g + 0.2f) : Mathf.Max(0, mr.material.color.g - 0.2f);
-                col.z = onoff ? Mathf.Min(1, mr.material.color.b + 0.2f) : Mathf.Max(0, mr.material.color.b - 0.2f);
-                mr.material.color = new Color(col.x, col.y, col.z);
-            }
-
-            col = this.transform.position;
-            col.y = onoff ? col.y - 0.2f : col.y + 0.2f;
-            this.transform.position = col;
-
-            //Collider coll = this.gameObject.GetComponent<Collider>();
-            //if (coll != null) coll.enabled = !onoff;
-       // }
-
-
+        col = this.transform.position;
+        col.y = onoff ? col.y - 0.2f : col.y + 0.2f;
+        this.transform.position = col;
     }
 
-    //private void OnEnable()
-    //{
-        
-    //}
 }
--- Assets/Scripts/TileBuilder.cs
+++ Assets/Scripts/TileBuilder.cs
@@ -38,7 +38,6 @@ public class TileBuilder : MonoBehaviour
         {
             //CreateGrid();
            // mytiles = CreateGrid(mytiles); // instant build (returns the built list to mytiles or any tiles List outside of this script) (should use this , but not here)
-            //StartCoroutine(CreateGridOneByOne(mytiles, buildStepTime)); //this is just to show in slow mo
         }
         tileSizeX = stepX;
         tileSizeY = stepY;
@@ -150,165 +149,113 @@ public class TileBuilder : MonoBehaviour
     }
 
     //Slow mo version , Only for show, or if needed as well
-    public IEnumerator CreateGridOneByOne(List<Tile> tiles , float time = 0.1f)
-    {
-
-        if (pooler == null)
-        {
-            Debug.LogError("Pooler not referenced, make sure the pooler is referenced on the Tile Builder Component");
-            yield return null;
-        }
-
-        if (tiles != null)
-        {
-            for (int i = 0; i < tiles.Count; i++)
-            {
-                tiles[i].gameObject.SetActive(false);
-            }
-        }
-        tiles = new List<Tile>();
-
-
-        if (hexGrid)
-        {
-            stepX *= 1.5f;
-            stepZ *= 0.5f;
-        }
-
-        cursor = new GameObject("cursor");
-        cursor.transform.position = this.transform.position;
-        float startPosX;
-        float startPosY;
-        float startPosZ;
-
-        if (boardSizeX % 2 == 0) startPosX = ((boardSizeX - 1) / 2) * (stepX + separation) + (stepX / 2 + separation / 2);
-        else startPosX = (boardSizeX / 2) * (stepX + separation);
-
-        if (boardSizeY % 2 == 0) startPosY = ((boardSizeY - 1) / 2) * (stepY + separation) + (stepY / 2 + separation / 2);
-        else startPosY = (boardSizeY / 2) * (stepY + separation);
-
-        if (boardSizeZ % 2 == 0) startPosZ = ((boardSizeZ - 1) / 2) * (stepZ + separation) + (stepZ / 2 + separation / 2);
-        else startPosZ = (boardSizeZ / 2) * (stepZ + separation);
-
-
-        cursor.transform.position = new Vector3(cursor.transform.position.x - startPosX, cursor.transform.position.y + startPosY, cursor.transform.position.z + startPosZ);
-        float startX = cursor.transform.position.x;
-        float startY = cursor.transform.position.y;
-        float startZ = cursor.transform.position.z;
-
-
-        Tile currentTile;
-        float step;
-        if (hexGrid)
-        {
-            boardSizeX += 1;
-            boardSizeZ -= 1;
-        }
-
-        for (int i = 0; i < boardSizeZ; i++)
-        {
-            for (int j = 0; j < boardSizeY; j++)
-            {
-                for (int k = 0; k < boardSizeX; k++)
-                {
-                    //if (applyHexShifting && (k == 0 /* boardSizeX - 1*/) && i % 2 == 0) continue;
-                    if (hexGrid && (k == boardSizeX - 1) && i % 2 == 0) continue;
-
-                    currentTile = pooler.Get(true).GetComponent<Tile>();
-                    tiles.Add(currentTile);
-
-                    currentTile.x = k;
-                    currentTile.y = j;
-                    currentTile.z = i;
-                    currentTile.listIndex = tiles.Count - 1;
-
-                    if (currentTile.coordText != null) currentTile.coordText.text = "x: " + k + "\nz: " + i;
-
-                    currentTile.transform.position = cursor.transform.position;
-
-                    step = stepX + separation;// applyHexShifting && i % 2 == 0 ? /*(stepX + separation) + */(stepX * 1.25f + separation * 1.25f) : stepX + separation;
-
-                    cursor.transform.position = new Vector3(cursor.transform.position.x + step, cursor.transform.position.y, cursor.transform.position.z);
-                    
-                    yield return new WaitForSeconds(time);
-
-                }
-                
-                cursor.transform.position = new Vector3(startX, cursor.transform.position.y - (stepY + separation), cursor.transform.position.z);
-            }
-            step = hexGrid && i % 2 == 0 ? -1 * (stepX * 0.5f + separation * 0.5f) : 0;
-            cursor.transform.position = new Vector3(startX + step, startY, cursor.transform.position.z - (stepZ + separation));
-        }
-        if (hexGrid)
-        {
-            boardSizeX -= 1;
-            boardSizeZ += 1;
-            stepX /= 1.5f;
-            stepZ /= 0.5f;
-
-        }
-        Debug.Log(tiles.Count);
-        for (int i = 0; i < tiles.Count; i++)
-        {
-            tiles[i].SetNeigbours(tiles);
-        }
-        TileBuilder.tiles = new List<Tile>(tiles);
-
-
-    }
-
-    //private void SetTilesNeighbors(List<Tile> tiles)
+    //public IEnumerator CreateGridOneByOne(List<Tile> tiles , float time = 0.1f)
     //{
-    //    if (tiles == null) return;
 
-    //    Tile tile;
-    //    for (int i = 0; i < tiles.Count; i++)
+    //    if (pooler == null)
     //    {
-    //        tile = tiles[i];
-    //        //tile.leftTiles = tiles.FindAll((t => t.z == tile.z && t.x < tile.x));
-    //        //tile.rightTiles = tiles.FindAll((t => t.z == tile.z && t.x > tile.x));
-    //        //tile.upTiles = tiles.FindAll((t => t.x == tile.x && t.z < tile.z));
-    //        //tile.downTiles = tiles.FindAll((t => t.x == tile.x && t.z > tile.z));
+    //        Debug.LogError("Pooler not referenced, make sure the pooler is referenced on the Tile Builder Component");
+    //        yield return null;
+    //    }
 
-    //        Tile temp;
-    //        for (int j = 1; j < 6; j++)
+    //    if (tiles != null)
+    //    {
+    //        for (int i = 0; i < tiles.Count; i++)
     //        {
-
-    //            temp = tiles.Find(t => (t.z == tile.z && t.x == tile.x - j));
-    //            if (temp != null && tile != temp) tile.leftTiles.Add(temp);
-    //            temp = null;
-
-    //            temp = tiles.Find(t => (t.z == tile.z && t.x == tile.x + j));
-    //            if (temp != null && tile != temp) tile.rightTiles.Add(temp);
-    //            temp = null;
-
-    //            temp = tiles.Find(t => (t.x == tile.x && t.z == tile.z + j));
-    //            if (temp != null && tile != temp) tile.downTiles.Add(temp);
-    //            temp = null;
-
-    //            temp = tiles.Find(t => (t.x == tile.x && t.z == tile.z - j));
-    //            if (temp != null && tile != temp) tile.upTiles.Add(temp);
-    //            temp = null;
-
-    //            temp = tiles.Find(t => (t.x == tile.x - j && t.z == tile.z - j && t.x != tile.x && t.z != tile.z));
-    //            if (temp != null && tile != temp) tile.northWestTiles.Add(temp);
-    //            temp = null;
-
-    //            temp = tiles.Find(t => (t.x == tile.x + j && t.z == tile.z - j && t.x != tile.x && t.z != tile.z));
-    //            if (temp != null && tile != temp) tile.northEastTiles.Add(temp);
-    //            temp = null;
-
-    //            temp = tiles.Find(t => (t.x == tile.x + j && t.z == tile.z + j && t.x != tile.x && t.z != tile.z));
-    //            if (temp != null && tile != temp) tile.southEastTiles.Add(temp);
-    //            temp = null;
-
-    //            temp = tiles.Find(t => (t.x == tile.x - j && t.z == tile.z + j && t.x != tile.x && t.z != tile.z));
-    //            if (temp != null && tile != temp) tile.southWestTiles.Add(temp);
-    //            temp = null;
+    //            tiles[i].gameObject.SetActive(false);
     //        }
+    //    }
+    //    tiles = new List<Tile>();
+
+
+    //    if (hexGrid)
+    //    {
+    //        stepX *= 1.5f;
+    //        stepZ *= 0.5f;
+    //    }
+
+    //    cursor = new GameObject("cursor");
+    //    cursor.transform.position = this.transform.position;
+    //    float startPosX;
+    //    float startPosY;
+    //    float startPosZ;
+
+    //    if (boardSizeX % 2 == 0) startPosX = ((boardSizeX - 1) / 2) * (stepX + separation) + (stepX / 2 + separation / 2);
+    //    else startPosX = (boardSizeX / 2) * (stepX + separation);
+
+    //    if (boardSizeY % 2 == 0) startPosY = ((boardSizeY - 1) / 2) * (stepY + separation) + (stepY / 2 + separation / 2);
+    //    else startPosY = (boardSizeY / 2) * (stepY + separation);
+
+    //    if (boardSizeZ % 2 == 0) startPosZ = ((boardSizeZ - 1) / 2) * (stepZ + separation) + (stepZ / 2 + separation / 2);
+    //    else startPosZ = (boardSizeZ / 2) * (stepZ + separation);
+
+
+    //    cursor.transform.position = new Vector3(cursor.transform.position.x - startPosX, cursor.transform.position.y + startPosY, cursor.transform.position.z + startPosZ);
+    //    float startX = cursor.transform.position.x;
+    //    float startY = cursor.transform.position.y;
+    //    float startZ = cursor.transform.position.z;
+
+
+    //    Tile currentTile;
+    //    float step;
+    //    if (hexGrid)
+    //    {
+    //        boardSizeX += 1;
+    //        boardSizeZ -= 1;
+    //    }
+
+    //    for (int i = 0; i < boardSizeZ; i++)
+    //    {
+    //        for (int j = 0; j < boardSizeY; j++)
+    //        {
+    //            for (int k = 0; k < boardSizeX; k++)
+    //            {
+    //                //if (applyHexShifting && (k == 0 /* boardSizeX - 1*/) && i % 2 == 0) continue;
+    //                if (hexGrid && (k == boardSizeX - 1) && i % 2 == 0) continue;
+
+    //                currentTile = pooler.Get(true).GetComponent<Tile>();
+    //                tiles.Add(currentTile);
+
+    //                currentTile.x = k;
+    //                currentTile.y = j;
+    //                currentTile.z = i;
+    //                currentTile.listIndex = tiles.Count - 1;
+
+    //                if (currentTile.coordText != null) currentTile.coordText.text = "x: " + k + "\nz: " + i;
+
+    //                currentTile.transform.position = cursor.transform.position;
+
+    //                step = stepX + separation;// applyHexShifting && i % 2 == 0 ? /*(stepX + separation) + */(stepX * 1.25f + separation * 1.25f) : stepX + separation;
+
+    //                cursor.transform.position = new Vector3(cursor.transform.position.x + step, cursor.transform.position.y, cursor.transform.position.z);
+                    
+    //                yield return new WaitForSeconds(time);
+
+    //            }
+                
+    //            cursor.transform.position = new Vector3(startX, cursor.transform.position.y - (stepY + separation), cursor.transform.position.z);
+    //        }
+    //        step = hexGrid && i % 2 == 0 ? -1 * (stepX * 0.5f + separation * 0.5f) : 0;
+    //        cursor.transform.position = new Vector3(startX + step, startY, cursor.transform.position.z - (stepZ + separation));
+    //    }
+    //    if (hexGrid)
+    //    {
+    //        boardSizeX -= 1;
+    //        boardSizeZ += 1;
+    //        stepX /= 1.5f;
+    //        stepZ /= 0.5f;
 
     //    }
+    //    Debug.Log(tiles.Count);
+    //    for (int i = 0; i < tiles.Count; i++)
+    //    {
+    //        tiles[i].SetNeigbours(tiles);
+    //    }
+    //    TileBuilder.tiles = new List<Tile>(tiles);
+
+
     //}
 
+    
 }
 
